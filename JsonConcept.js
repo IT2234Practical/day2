b4afa1e72c375c3b7c027d8a1e4274ffc87103c7
isuruamarasooriya <isuruamarasooriya177@gmail.com>
2025-03-18 22:55:20 +0530

    Create JsonConcept.js

---
+++ JsonConcept.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+//JSON
+//{key:value}
+
+let student = {regno:'2021/ICT/42', name:'Isuru', age:23, course:'IT'}
+	console.log(student) //Output : {regno:'2021/ICT/42', name:'Isuru', age:23, course:'IT'}
+	console.log(student.name) //Output : Isuru
+	
+
+let students = [
+{regno:'2021/ICT/42', name:'Isuru', age:23, course:'IT'},
+{regno:'2021/ICT/122', name:'Kanishka', age:23, course:'IT'},
+{regno:'2021/ICT/45', name:'Tharusha', age:23, course:'IT'}
+]
+
+	console.log(students) 
+		/*Output : [
+					  { regno: '2021/ICT/42', name: 'Isuru', age: 23, course: 'IT' },
+					  { regno: '2021/ICT/122', name: 'Kanishka', age: 23, course: 'IT' },
+					  { regno: '2021/ICT/45', name: 'Tharusha', age: 23, course: 'IT' }
+					]
+		*/
+	console.log(students.name) //Output: undefined
